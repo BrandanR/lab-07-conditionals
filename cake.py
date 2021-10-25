7bc477023bca50ff68d4264b3866107a8e0f7273
Brandan Ramnandan <brandanramnandan@Brandans-Air.fios-router.home>
2021-10-25 16:24:01 -0400

    finished cake files and html version of oddcalc

---
+++ cake.py
@@ -0,0 +1,7 @@
+cake = input("Do you want cake? (yes or no)")
+if cake == "yes" or cake == "Yes" or cake == "y" or cake == "yea" or cake == "Y":
+    print("Have some cake.")
+elif cake == "no" or cake == "No" or cake == "n" or cake == "naa" or cake == "N":
+    print("No cake for you!!!!")
+else:
+    print("ERROR : Sorry, I don't understand!")
